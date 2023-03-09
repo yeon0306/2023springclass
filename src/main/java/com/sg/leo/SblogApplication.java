752f803e0b8d880dc0d5db699db9bb93947b9657
310-29 <310-29@DESKTOP-NMAS9PK>
2023-03-09 13:43:01 +0900

    두번째 커밋, 소스 약간 수정

---
+++ src/main/java/com/sg/leo/SblogApplication.java
@@ -0,0 +1,16 @@
+package com.sg.leo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SblogApplication.class, args);
+	}
+	
+	// 2023.3.9 스프링수업시간에 만듬
+	// 박혜연
+	// 깃허브확인을 위해 한줄을 추가했습니다.
+}

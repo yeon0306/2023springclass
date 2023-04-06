1379731845f5c8c341d1f255111e4bfdc6a710d3
310-29 <310-29@DESKTOP-NMAS9PK>
2023-04-06 14:59:17 +0900

    commit

---
+++ tblog/src/main/java/com/sg/leo/exception/TBlogException.java
@@ -0,0 +1,9 @@
+package com.sg.leo.exception;
+
+public class TBlogException extends RuntimeException{
+   private static final long seriaLVersionUID = 1L;
+   
+   public TBlogException(String message) {
+      super(message);
+   }
+}
